6dd8195231709ee354d9f2a1ac55f660aafbf7e6
MrDeveloper <117296386+mr-developer-88@users.noreply.github.com>
2024-05-18 14:58:18 +0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+//   SHOW MENU
+
+
+// REMOVE MOBILE MENU
+
+
+// SCROLL SECTION ACTIVE LINK
+
+
+// CHANGE BACKGROUND HEADER 
+
+
+// DARK LIGHT THEME
+
+
+// previously selected topic
+
+
+//WE obbtain the current theme
+
+
+// validation
+
+
+// ACTIVATE / DE-ACTIVATE the same manually with button
+
+
+// SCROLL REVEAL ANIMATION
